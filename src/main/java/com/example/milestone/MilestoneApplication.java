b41d49569b1c89eb4728acab5c517e6521b0b7e3
ingDianaVaca <ingdianavaca@gmail.com>
2024-12-14 13:57:58 -0800

    First commit

---
+++ src/main/java/com/example/milestone/MilestoneApplication.java
@@ -0,0 +1,12 @@
+package com.example.milestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/milestone/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.milestone.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health () {
+        return "Testing...! ";
+    }
+}
